8f1a3a993182d4c6b991a887262c189be4a087f5
Jason <jasonm4130@gmail.com>
2019-06-02 12:13:53 +1000

    backend complete for now

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,69 +1,11 @@
-const Shift = require('../../models/shift');
-const Staff = require('../../models/staff');
+const shiftResolvers = require('./shifts');
+const staffMemberResolvers = require('./staffMember');
 
-module.exports = {
+const resolvers = {
 
-    shifts: () => {
-        return Shift
-            .find()
-            .populate('assignedStaff')
-            .then(shifts => {
-                return shifts.map(shift => {
-                    return {
-                        ...shift._doc
-                    };
-                });
-            })
-            .catch(err => {
-                throw err;
-            });
-    },
-
-    createShift: (args) => {
-        const shift = new Shift({
-            day: args.shiftInput.day,
-            description: args.shiftInput.description,
-            hours: +args.shiftInput.hours
-        });
-
-        return shift
-            .save()
-            .then(result => {
-                return {...result._doc};
-            })
-            .catch(err => {
-                throw err;
-            });
-    },
-
-    staff: () => {
-        return Staff
-            .find()
-            .populate('assignedShifts')
-            .then(staffs => {
-                return staffs.map(staff => {
-                    return {...staff._doc};
-                });
-            })
-            .catch(err => {
-                throw err;
-            });
-    },
-
-    createStaff: (args) => {
-        const staff = new Staff({
-            name: args.staffInput.name,
-            maxHours: +args.staffInput.maxHours
-        });
-
-        return staff
-            .save()
-            .then(result => {
-                return {...result._doc}
-            })
-            .catch(err => {
-                throw err;
-            });
-    }
+    ...shiftResolvers,
+    ...staffMemberResolvers
 
 };
+
+module.exports = resolvers;
